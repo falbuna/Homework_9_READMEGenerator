5f4822a77d8eb9331206829a9fd36626d063d0e8
Francis Albuna <falbuna1@gmail.com>
2020-08-28 18:21:28 -0700

    Installed the inquirer npm. Added inquirer and 2 questions to ask.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 // array of questions for user
 const questions = [
-
+    {
+        name: 'Title',
+        message: 'What is the title of your Application?'
+    },
+    {
+        name: 'Description',
+        message: 'Describe the Project'
+    }
 ];
 
 // function to write README file
@@ -9,7 +19,7 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions)
 }
 
 // function call to initialize program
