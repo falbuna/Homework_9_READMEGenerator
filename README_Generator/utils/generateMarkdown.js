356b654d022856c461601d66e86a0afa651fd964
Francis Albuna <falbuna1@gmail.com>
2020-08-30 15:26:03 -0700

    Fixed license choices. Added more content to table of contents and added breaks.

--- README_Generator/utils/generateMarkdown.js
+++ README_Generator/utils/generateMarkdown.js
@@ -3,8 +3,13 @@ function generateMarkdown(data) {
   return `# ${data.title}\n
 ![License|${data.license}](https://img.shields.io/badge/License-${data.license}-blue)\n
 ## Table of Contents
-[Description](#Description)
-[Installation](#Installation)
+[Description](#Description)\n
+[Installation](#Installation)\n
+[Usage](#Usage)\n
+[License](#License)\n
+[Contributing](#Contributing)\n
+[Tests](#Tests)\n
+[Questions](#Questions)\n
 ## Description
 ${data.description}\n
 ## Installation
